f7801cbb1468f3a39868a9f082e918477d3b4b39
leomazzeu <leonardomazzeudasilva@gmail.com>
2022-08-20 17:29:04 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_MVC_DOTNET6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_MVC_DOTNET6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
